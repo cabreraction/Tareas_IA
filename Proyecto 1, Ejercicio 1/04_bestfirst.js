f009fa42adc9eb2540c43f2c9453beeb8c1a0d20
Javier A. Cabrera <cabreraction@gmail.com>
2022-01-02 09:05:11 -0600

    finished proyecto 1

--- Proyecto 1, Ejercicio 1/04_bestfirst.js
+++ Proyecto 1, Ejercicio 1/04_bestfirst.js
@@ -1,5 +1,6 @@
 // MIT License
 // Copyright (c) 2021 Luis Espino
+let moves = 0;
 
 function heuristic(start, end, h) {
 	if (h == 1)	{ // tiles out is sometimes encycled
@@ -25,25 +26,27 @@ function successors(n, e, h){
 		let child = n[0].substring(0,i)+n[0].substring(i+1,i+2)+tmp+n[0].substring(i+2)
 		suc.push([child,heuristic(child, e, h),inc()])
 	}
-	return suc
+	suc.sort((first, second) => first[1] - second[1]);
+	let seccondSuccesors = []
+	seccondSuccesors.push(suc[0])
+	return seccondSuccesors
 }
 
-function bestfirst(start, end, h){
+function hills(start, end, h){
 	var cont = 0
 	var dot = '{'
 	var list = [[start,heuristic(start, end, h),inc()]];
 	dot+=list[0][2]+' [label="'+list[0][0]+'"];'
-	while (list.length > 0){		
+	while (list.length > 0 && moves < 20){		
 		var current = list.shift();
 		if (current[0] == end) {			
 			dot += '}'
 			return dot
 		}		
 		var temp = successors(current, end, h);
-		//temp.reverse();
 		temp.forEach(val => dot+=val[2]+' [label="'+val[0]+'"];'+current[2]+'--'+val[2]+' [label="'+val[1]+'"] ;')
 		list = list.concat(temp);
-		list = list.sort( function(a,b) { return a[1] - b[1] });
+		moves++ 
 		cont++
 		if (cont > 100) {
 			alert("The search is looped!")
@@ -64,5 +67,5 @@ function puzzle() {
 	var nodes = prompt("Ingrese texto inicial, texto final y heurstica (1 o 2) separados por un espacio")
 	if (nodes == null || nodes == '') nodes = 'halo hola 1'
 	nodes = nodes.split(' ')
-	return bestfirst(nodes[0], nodes[1], nodes[2])
+	return hills(nodes[0], nodes[1], nodes[2])
 }
